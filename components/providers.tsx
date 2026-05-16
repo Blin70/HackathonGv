0b33263a05aa46e840d412eb90656b164a9d4a66
Blin Axhirexha <blini.axhirexha@gmail.com>
2026-05-17 05:43:26 +1300

    Providers file

---
+++ components/providers.tsx
@@ -0,0 +1,7 @@
+import { TooltipProvider } from "./ui/tooltip"
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <TooltipProvider>{children}</TooltipProvider>
+    )
+}
